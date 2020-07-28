04a816225e772ca3ef7cc5838144da5cd0ea28b9
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-28 02:19:56 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,22 @@
+class Appointment
+
+  attr_accessor :date, :patient, :doctor
+  @@all = []
+
+  def initialize(patient, date, doctor)
+    @patient = patient
+    @date = date
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def patients 
+    self.select {|appointment| appointment.patient}
+  end 
+  
+end 
+
